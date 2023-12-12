7e1a8675d9b701593f3898071ed3728eb1df0e5e
Neal <darley.neal@gmail.com>
2023-12-11 20:24:31 -0700

    D11, Pt01

--- Day11-CosmicExpansion/cosmic_expansion_pt1.py
+++ Day11-CosmicExpansion/cosmic_expansion_pt1.py
@@ -1,44 +1,102 @@
+import time
 
+class Galaxy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 def main():
-    galaxy_map = []
-    galaxies = {}
-    galaxies_x = {}
-    with open("test_input.txt") as f:
+    start = time.time()
+
+    galaxies_array = []
+    galaxies_map = {}
+    input_width = 0
+    input_height = 0
+    with open("input.txt") as f:
         input = f.readlines()
+        input_width = len(input[0].strip("\n"))
+        input_height = len(input)
         for i in range(len(input)):
             line = input[i].strip("\n")
-            galaxy_map.append(line)
             for j in range(len(line)):
                 if line[j] == "#":
-                    if i not in galaxies:
-                        galaxies[i] = []
-                    galaxies[i].append(j)
-                    galaxies_x[j] = 1
+                    if i not in galaxies_map:
+                        galaxies_map[i] = [j]
+                    else:
+                        galaxies_map[i].append(j)
+
+                    galaxies_array.append(Galaxy(j, i))
     
-    # find the rows that have no galaxies, and insert another row of '.'s
-    for i in range(len(galaxy_map)):
-        if i not in galaxies:
-            galaxy_map.insert(i, "." * len(galaxy_map[0]))
+    horizontal_spaces, vertical_spaces = find_spaces(input_width, input_height, galaxies_map)
+    expand_spaces(horizontal_spaces, vertical_spaces, galaxies_array)
+    manhattan_distances = find_manhattan_distances(galaxies_array)
 
-    # find the columns that have no galaxies, and insert another column of '.'s
-    for i in range(len(galaxy_map[0])):
-        if i not in galaxies_x:
-            for j in range(len(galaxy_map)):
-                galaxy_map[j] = galaxy_map[j][:i] + "." + galaxy_map[j][i:]
+    print(f'Distance between all galaxies: {sum(manhattan_distances)}')
+    end = time.time()
+    print(f'Time: {end - start}')
 
-    #adjust the galaxies dictionary to account for the new rows and columns
-    galaxies = {}
-    for i in range(len(galaxy_map)):
-        for j in range(len(galaxy_map[i])):
-            if galaxy_map[i][j] == "#":
-                if i not in galaxies:
-                    galaxies[i] = []
-                galaxies[i].append(j)
+def find_spaces(width, height, map):
+    horizontal_spaces = []
+    vertical_spaces = []
+    for i in range(height):
+        if i not in map:
+            horizontal_spaces.append(i)
 
-    # Print the new galaxy map
-    for line in galaxy_map:
-        print(line)
+    for i in range(width):
+        has_vertical_galaxy = False
+        for key in map:
+            if i in map[key]:
+                has_vertical_galaxy = True
+                break
+
+        if not has_vertical_galaxy:
+            vertical_spaces.append(i)
+
+    return horizontal_spaces, vertical_spaces
+
+def expand_spaces(horizontal_spaces, vertical_spaces, galaxies_array):
+    for galaxy in galaxies_array:
+        y_increment = 0
+        x_increment = 0
+        for i in range(len(horizontal_spaces)):
+            if galaxy.y > horizontal_spaces[i]:
+                y_increment += 1
+
+        for i in range(len(vertical_spaces)):
+            if galaxy.x > vertical_spaces[i]:
+                x_increment += 1
+
+        galaxy.y += y_increment
+        galaxy.x += x_increment
+
+def find_manhattan_distances(galaxies_array):
+    manhattan_distances = []
+    for i in range(len(galaxies_array)):
+        for j in range(i + 1, len(galaxies_array)):
+            manhattan_distances.append(abs(galaxies_array[i].x - galaxies_array[j].x) + abs(galaxies_array[i].y - galaxies_array[j].y))
+
+    return manhattan_distances
+
+def print_galaxies(galaxies_array):
+    max_x = 0
+    max_y = 0
+    for galaxy in galaxies_array:
+        if galaxy.x > max_x:
+            max_x = galaxy.x
+        if galaxy.y > max_y:
+            max_y = galaxy.y
+
+    map = []
+    for i in range(max_y + 1):
+        map.append([])
+        for j in range(max_x + 1):
+            map[i].append(".")
+
+    for galaxy in galaxies_array:
+        map[galaxy.y][galaxy.x] = "#"
+
+    for i in range(len(map)):
+        print("".join(map[i]))
 
 
 main()
